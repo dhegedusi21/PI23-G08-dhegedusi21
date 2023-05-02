47a39f5633346d2022cae25b3c053885074179e9
Programsko Inzenjerstvo <pi_foi@outlook.com>
2023-05-02 13:28:22 +0200

    Forma studenata i klase

---
+++ Evaluation Manager/Models/Teacher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation_Manager.Models {
+    public class Teacher : Person{
+        public string Username {
+            get; set;
+        }
+        public string Password {
+            get; set;
+        }
+
+    } // public clas Teacher : Person
+}
